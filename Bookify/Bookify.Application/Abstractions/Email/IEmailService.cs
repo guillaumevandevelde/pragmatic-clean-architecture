34ae221d4d786016aa6e225412eb2fc7dd283335
Van de Velde Guillaume <vdvguillaume@gmail.com>
2024-12-26 10:59:08 +0100

    Add application layer

---
+++ Bookify/Bookify.Application/Abstractions/Email/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace Bookify.Application.Abstractions.Email;
+
+public interface IEmailService
+{
+    Task SendAsync(Domain.Users.Entities.Email recipient, string subject, string body);
+}
---
+++ Bookify/Bookify.Domain/Bookings/Services/IBookingRepository.cs
@@ -0,0 +1,17 @@
+﻿using Bookify.Domain.Apartments;
+using Bookify.Domain.Apartments.Entities;
+using Bookify.Domain.Bookings.Entities;
+
+namespace Bookify.Domain.Bookings;
+
+public interface IBookingRepository
+{
+    Task<Booking?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task<bool> IsOverlappingAsync(
+        Apartment apartment,
+        DateRange duration,
+        CancellationToken cancellationToken = default);
+
+    void Add(Booking booking);
+}
